3e60570de937ef07d5fa80c2fb0cac99a2acc518
Tim A. Conner <timaconner1@gmail.com>
2017-12-07 15:42:18 -0600

    Added sandwichView.

---
+++ js/sandwichView.js
@@ -0,0 +1,31 @@
+"use strict";
+
+module.exports.createDropDown = (ingredientsArray) => {
+    let output = document.getElementById("ingredients");
+    
+    for(let prop in ingredientsArray){
+        let ingredients = ingredientsArray[prop].getTypes();
+
+        let labelDOM = document.createElement("label");
+        let labelText = document.createTextNode(prop);
+        labelDOM.setAttribute("for", prop);
+        labelDOM.appendChild(labelText);
+
+        let selectDOM = document.createElement("select");
+        selectDOM.id = prop;
+        for(let ingredient in ingredients){
+            let option = document.createElement("option");
+            option.innerText = ingredient;
+            option.id = ingredient;
+            selectDOM.appendChild(option);
+        }
+
+        output.appendChild(labelDOM);
+        output.appendChild(selectDOM);
+    }
+};
+
+module.exports.viewPrice = (price) => {
+    console.log("hello");
+    document.getElementById("final-order").innerHTML = price.toFixed(2);
+};
